6167882979c9222d32b71448ec69af32300d6f65
Tạ Xuân Chiến; HN-JPN-KS24C; lập trình scratch; session 3 <xuanchien319@gmail.com>
2024-11-22 20:49:17 +0700

    session 4 cơ bản 1

---
+++ session 4 coban 1.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	int number;
+	printf("hay nhap mot so tu nhien ");
+	scanf("%d",&number);
+	if(number>=0){
+		printf("so nhap vao la so duong");
+		}
+		else{
+			printf("so nhap vao la so am");
+			}
+			return 0;
+}
+			
